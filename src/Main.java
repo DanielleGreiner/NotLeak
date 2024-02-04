dcb1ed5f46a3284b4fe9063a868bcb2143128c29
DanielleGreiner <dmgreiner4@icloud.com>
2024-02-04 18:59:41 -0500

    better way to store secrets

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Gather info from user");
+
+System.out.println("Insert into database");
+
+System.out.println("Connect to DB with " + Secret.uname + ":" + Secret.pass);
+	}
+
+}
